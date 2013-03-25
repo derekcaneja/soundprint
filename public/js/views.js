0da1d814480eb4e4228879201150179e8b0a7c0a
derekcaneja <d.caneja1012@gmail.com>
2013-03-25 04:07:49 -0500

    Backbone added

---
+++ public/js/views.js
@@ -0,0 +1,20 @@
+//-----------------------------------------//
+//------------------Views------------------//
+//-----------------------------------------//
+
+var Application = Backbone.View.extend({
+	tagName: 'div',
+	className: 'application',
+	initialize: function() {
+
+	},
+	render: function() {
+
+	},
+	events: {
+
+	},
+	resize: function() {
+		
+	}
+});
